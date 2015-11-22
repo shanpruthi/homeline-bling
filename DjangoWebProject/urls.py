c7c06154b65d8c96003852202ce0a82c3b4401cd
eexie <emmawfx4@gmail.com>
2015-11-22 10:05:32 -0600

    Update urls.py

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
+    url(r'^shelters', 'app.views.shelters', name='shelters'),
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {
