59c24094a959d406311c711d98d261640aa947f3
Shan Pruthi <shanpruthi@msn.com>
2015-11-22 11:57:25 -0600

    a

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -39,8 +39,6 @@ urlpatterns = patterns('',
         },
         name='logout'),
     url(r'^gather/$', 'app.views.gather_digits'),
-
-    url(r'^gather/$', 'app.views.gather_digits'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
