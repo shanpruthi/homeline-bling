816b9df925d1ebe7b707a95d0155ab419c0c9d7d
Qile Wang <wangqile123@gmail.com>
2015-11-22 02:19:42 -0600

    FOUND THE BLOODY ERROR

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -131,7 +131,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'twilio',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
