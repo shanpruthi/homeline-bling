0986651eba6e0c6e128b25b4eb853f06eb97dd60
Qile Wang <wangqile123@gmail.com>
2015-11-22 11:48:10 -0600

    added the nearest function to views

--- app/views.py
+++ app/views.py
@@ -14,18 +14,38 @@ from twilio.twiml import Response
 
 import json, requests
 
+
+
 ######## Helper Functions ############
+
 
 def geocoding(location):
     return "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyA7XxD_9Z_7uJpi5Jx0sXFe_dT3NL2qpCk"
 
-def get_closest_shelter(location):
+def get_user_location(location):
     reqSent = requests.post(geocoding(location))
     jsonSent = json.loads(reqSent.content)
     latitude = jsonSent['results'][0]['geometry']['location']['lat']
     longitude = jsonSent['results'][0]['geometry']['location']['lng']
 
     return {'latitude': latitude, 'longitude': longitude}
+
+def get_closest_shelter(location):
+    shelters = Shelter_Information.objects.filter(spots_remaining__gt=0).values('longitude', 'latitude', 'shelter_name', 'shelter_address')
+    current_location = get_user_location(location)
+
+    closest_name = ""
+    closest_address = ""
+
+    shortest = 10000
+    for key in shelters:
+        length = sqrt(pow(shelters[key].latitude-current_location['latitude'],2) + pow(shelters[key].longitude - current_location['longitude'],2))
+        if length < shortest:
+            closest_name = shelters[key].shelter_name
+            closest_address = shelters[key].shelter_address
+
+    return [closest_name, closest_address]
+
 
 ######## View Functions ##############
 
@@ -102,8 +122,8 @@ def login(request):
 def gather_digits(request):
     twilio_response = Response()
     city = request.POST.get('FromCity', '')
-    shelters = Shelter_Information.objects.filter(spots_remaining__gt=0)
-    shelter = get_closest_shelter(city)
-    twilio_response.say("You are from %s" % shelter)
+    
+    #shelter = get_closest_shelter(city)
+    twilio_response.say("Hello")
 
     return twilio_response
