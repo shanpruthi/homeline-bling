19e23d4b823418eea73936ae54893b721fcf76d2
Qile Wang <wangqile123@gmail.com>
2015-11-22 07:47:17 -0600

    Make sure sms is going through

--- app/views.py
+++ app/views.py
@@ -56,9 +56,9 @@ def gather_digits(request):
     twilio_response = Response()
  
     with twilio_response.gather(action='/respond/', numDigits=1) as g:
-        g.say('Press one to hear a song, two to receive an SMS')
+        g.say('Press one to be dissapointed, two to receive an SMS')
         g.pause(length=1)
-        g.say('Press one to hear a song, two to receive an SMS')
+        g.say('Press one to be dissapointed, two to receive an SMS')
  
     return twilio_response
 
@@ -67,7 +67,7 @@ def handle_response(request):
     digits = request.POST.get('Digits', '')
     twilio_response = Response()
     if digits == '1':
-        twilio_response.play('http://bit.ly/phaltsw')
+        twilio_response.say("Hah, you don't get a song")
  
     if digits == '2':
         number = request.POST.get('From', '')
