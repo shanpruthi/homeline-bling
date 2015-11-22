85e01c997f1e8a0f01d11ae50df0872800036d57
Qile Wang <wangqile123@gmail.com>
2015-11-22 02:57:14 -0600

    virtualenv added. Please work

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
-#from twilio.twiml import Response
+from twilio.twiml import Response
 
 def home(request):
     """Renders the home page."""
